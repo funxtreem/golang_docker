04c9765328401b4a6d97deebad4be2df8eef013a
cakramining2021 <cakramining2021@gmail.com>
2023-11-01 20:27:53 +0800

    First Commit

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func main() {
+	var err error
+	db, err = gorm.Open(mysql.Open("root:cakratendados@tcp(localhost:3306)/db_coba"))
+	if err != nil {
+		fmt.Println("Connection Failed to Open")
+	} else {
+		fmt.Println("Go Docker Running")
+		fmt.Println("Connection Established")
+		fmt.Println("This Service Running on Port: 3000")
+		HandleRequest()
+	}
+
+}
+
+func SelectTable1(c *gin.Context) {
+	type TableStruct struct {
+		Id     uint64 `json:"id"`
+		Number int    `json:"number"`
+		Text   string `json:"string"`
+	}
+	var DataOutput []TableStruct
+
+	result := db.Table("table1").Select("*").Scan(&DataOutput)
+
+	if result.RowsAffected > 0 {
+		c.JSON(http.StatusOK, DataOutput)
+	} else {
+		c.JSON(http.StatusOK, "Error")
+	}
+
+}
+
+func SelectTable2(c *gin.Context) {
+	type TableStruct struct {
+		Code string `json:"code"`
+		Name string `json:"name"`
+	}
+	var DataOutput []TableStruct
+
+	result := db.Table("table2").Select("*").Scan(&DataOutput)
+
+	if result.RowsAffected > 0 {
+		c.JSON(http.StatusOK, DataOutput)
+	} else {
+		c.JSON(http.StatusOK, "Error")
+	}
+
+}
+
+func HandleRequest() {
+	myRouter := gin.Default()
+	myRouter.GET("/SelectTable1", SelectTable1)
+	myRouter.GET("/SelectTable2", SelectTable2)
+
+	err := myRouter.Run(":9000")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
